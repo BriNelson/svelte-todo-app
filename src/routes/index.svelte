af0964e9e0af1c18cd18cc299ca48ec7dee91171
brain <ladofmusi@outlook.com>
2022-03-04 21:32:15 -0700

    initial commit + form stuff

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+function handleClick() {
+
+    console.log('the click listener is working')
+  }
+</script>
+
+<form on:submit|preventDefault={handleClick}>
+    <input type="text" value="task">
+    <input type="text" value="category">
+    <button>Add Task</button>
+</form>
+
+<ul>
+  <li></li>
+</ul>
+
+<style>
+
+</style>
