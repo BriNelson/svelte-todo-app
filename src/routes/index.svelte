565da13bae743c7f5ef4ae51c57dc96045cb6af9
Brinelson <ladofmusic@gmail.com>
2022-03-06 17:31:33 -0700

    app sort of done need to get parcel running

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,46 @@
 <script>
-function handleClick() {
 
+  let newTodo = ''
+  let todoList=[
+    {todo: "work" , competed: false},
+    {todo: "play" , competed: false},
+    {todo: "run" , competed: false},
+  ]
+function handleClick() {
+todoList = [...todoList, {todo: newTodo, competed: false}]
+    console.log(todoList)
     console.log('the click listener is working')
+    newTodo = ''
+  }
+  function removeTodo(index) {
+    todoList.splice(index, 1)
+    todoList = todoList
   }
 </script>
-
+<div class="container">
 <form on:submit|preventDefault={handleClick}>
-    <input type="text" value="task">
-    <input type="text" value="category">
-    <button>Add Task</button>
+  <div class="row">
+    <div class="col">
+    <input class="form-control" type="text" bind:value={newTodo}>
+    </div>
+    <div class = "col">
+
+    <button class="btn btn-primary" >Add Task</button>
+  </div>
+
+</div>
 </form>
 
-<ul>
-  <li></li>
-</ul>
+
+{#each todoList as item, index}
+
+  <div class="">
+  {item.todo}<button type="button" class="btn btn-danger" on:click={()=>removeTodo(index)} >delete</button>
+  </div>
+{/each}
+</div>
 
 <style>
+
 
 </style>
